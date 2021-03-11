4510bc90da71280629596a1f9c62518c0b79afcb
CHINNAPATH <78593342+CHINNAPATH@users.noreply.github.com>
2021-03-11 23:33:57 +0700

    Add files via upload

---
+++ work3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace work3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mode :");
+            string mode;
+            double x, y;
+            mode = Console.ReadLine();
+            Console.Write("X :");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("Y :");
+            y = double.Parse(Console.ReadLine());
+            if (x >= 0)
+            {
+                switch (mode)
+                {
+                    case "time":
+                        y = Math.Pow(x - 1, 2);
+                        Console.WriteLine("Time :{0}", x);
+                        Console.WriteLine("Price :{0}", y);
+                        break;
+                    case "price":
+                        x = Math.Sqrt(y) + 1;
+                        Console.WriteLine("Time :{0}", x);
+                        Console.WriteLine("Price :{0}", y);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid mode");
+                        break;
+                }
+            }
+            else if (x < 0)
+            {
+                Console.WriteLine("Invalid mouse position");
+            }
+
+        }
+    }
+}
